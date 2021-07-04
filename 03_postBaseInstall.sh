ad749bd691fb4251a21a97e6258386e4f0bfca05
quantumsnippet <darshan.haragi9@gmail.com>
2021-07-04 21:06:05 +0530

    Fixed the visudo error, parallel downloads and added --noconfirm flag for all pacman commands

--- 03_postBaseInstall.sh
+++ 03_postBaseInstall.sh
@@ -1,11 +1,9 @@
 echo """"""ADDING USER AND GIVING SUDO PRIVILEGES""""""
 useradd darshan
 passwd darshan
-
-echo """"""Uncomment the wheel group. It is the line which begins with wheel and save it""""""
-sleep 10
-visudo
 usermod -aG wheel darshan
+sed '/wheel ALL=(ALL) ALL/s/^#//' -i /etc/sudoers
+#EDITOR=vim visudo
 
 su darshan
 groups
@@ -15,7 +13,8 @@ sleep 5
 echo "-------------------------------------------------"
 
 
-echo """"""INCLUDING MULTILIB REPOSITORY""""""
+echo """"""INCLUDING MULTILIB REPOSITORY AND ENABLING PARALLEL DOWNLOADS""""""
+sed '/ParallelDownloads = 5/s/^#//' -i /etc/pacman.conf
 echo """"""Uncomment the [multilib] repository line, its mirrorlist include line and save it""""""
 sleep 10
 sudo vim /etc/pacman.conf
@@ -23,36 +22,35 @@ echo "-------------------------------------------------"
 
 
 echo """"""UPDATING THE REPOSITORIES""""""
-sudo pacman -Syyu
+sudo pacman -Syyu --noconfirm
 echo "-------------------------------------------------"
 
 
 echo """"""INSTALLATION OF XORG, DESKTOP ENVIRONMENT AND LOGIN DISPLAY MANAGER""""""
-sudo pacman -S xorg
-sudo pacman -S mate mate-extra
-sudo pacman -S lxdm
+sudo pacman -S xorg --noconfirm
+sudo pacman -S mate mate-extra --noconfirm
+sudo pacman -S lxdm --noconfirm
 sudo systemctl enable lxdm.service
 echo "-------------------------------------------------"
 
 
 echo """"""WIFI CONNECTION""""""
-sudo pacman -S broadcom-wl-dkms network-manager-applet
+sudo pacman -S broadcom-wl-dkms network-manager-applet --noconfirm
 sudo rmmod b43 b43legacy bcm43xx bcma brcm80211 brcmfmac brcmsmac ssb wl
 sudo modprobe wl
 echo "-------------------------------------------------"
 
 
 echo """"""INSTALLING MY NECESSARY PACKAGES""""""
-sudo pacman -S pacman-contrib p7zip unrar zip unzip gvfs gvfs-mtp youtube-dl vlc libreoffice-fresh firefox htop neofetch gdb geany bleachbit ranger git curl wget gvim vim-airline vim-airline-themes vim-syntastic qbittorrent xfce4-terminal
+sudo pacman -S pacman-contrib p7zip unrar zip unzip gvfs gvfs-mtp youtube-dl vlc libreoffice-fresh firefox htop neofetch gdb geany bleachbit ranger git curl wget gvim vim-airline vim-airline-themes vim-syntastic qbittorrent xfce4-terminal --noconfirm
 echo "-------------------------------------------------"
 
 
 echo """"""INSTALLING PACKAGES FOR PLAYING SOUND""""""
-sudo pacman -S pulseaudio pulseaudio-alsa pavucontrol alsa-utils alsa-plugins alsa-lib alsa-firmware gstreamer volumeicon
+sudo pacman -S pulseaudio pulseaudio-alsa pavucontrol alsa-utils alsa-plugins alsa-lib alsa-firmware gstreamer volumeicon --noconfirm
 echo "-------------------------------------------------"
 
 
 echo """"""REBOOT""""""
 sleep 5
-reboot
 reboot
